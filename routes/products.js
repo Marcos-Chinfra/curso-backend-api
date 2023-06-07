4fcf87a6aa4bb3478b916ed67f91721aa4932a11
MSCH13 <marcoschinchilla13@gmail.com>
2023-06-06 20:29:54 -0600

    empezando servicios

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
-
+const ProductsService = require('../services/products.service');
 const router = express.Router();
+const service = new ProductsService();
+
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for(let i = 0; i < limit; i++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -23,11 +15,45 @@ router.get('/filter', (req, res) =>{
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
+  const product = service.findOne(id);
+  res.json(product)
+});
+
+router.post('/', (req, res) =>{
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  })
+})
+
+router.patch('/:id', (req, res) =>{
+  const { id } = req.params;
+  const body = req.body;
   res.json({
+    message: 'update',
+    data: body,
     id,
-    name: 'Producto 1',
-    price: 100
   });
 });
+
+router.delete('/:id', (req, res) =>{
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
+// router.put('/:id', (req, res) =>{
+//   const { id } = res.params;
+//   const body = req.body;
+//   res.json({
+//     message: 'update',
+//     data: body,
+//     id,
+//   });
+// });
+
 
 module.exports = router;
